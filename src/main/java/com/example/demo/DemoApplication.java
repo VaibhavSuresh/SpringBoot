64dc820237bc00d830872ff23ccf5935b7edd441
VaibhavSuresh <suresh.april.1991@gmail.com>
2022-08-03 19:52:43 +0530

    commit comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/*
+	 * this is my first code commit
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
